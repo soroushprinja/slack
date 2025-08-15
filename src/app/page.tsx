6e596a88acc750ea7ed4699a8036b671f193f5c1
SOROUSH PRINJA <soroushprinja200@gmail.com>
2025-08-16 02:06:59 +0530

    15 aug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
-
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
     return(
       <>
+      <AuthScreen/>
       <Button variant="slack">click me</Button>
       <div className="text-rose-500 font-bold text-2xl">Hello World</div>
       </>
