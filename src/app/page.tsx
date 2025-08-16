1542eeabe733481ffc9134d45f7692094f6f7124
SOROUSH PRINJA <soroushprinja200@gmail.com>
2025-08-16 19:54:57 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
     return(
       <>
       <AuthScreen/>
-      <Button variant="slack">click me</Button>
-      <div className="text-rose-500 font-bold text-2xl">Hello World</div>
+      
       </>
     );
 
